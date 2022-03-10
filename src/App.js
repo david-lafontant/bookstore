6188420c55896816bac13d038e372eeb09b1211e
david-lafontant <lafontant.david@gmail.com>
2022-03-09 19:37:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Books from './components/Books';
+import Categories from './components/Categories';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <section className="content">
+      <Routes>
+        <Route path="/bookstore" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/*" element={<Books />} />
+      </Routes>
+    </section>
+  </Router>
+);
 
 export default App;
