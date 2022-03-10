70f11d19012b48f0b6ce63a8c5808b31086ca93b
david-lafontant <lafontant.david@gmail.com>
2022-03-09 22:58:13 -0600

    Update ItemBook.js

--- src/components/ItemBook.js
+++ src/components/ItemBook.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/books';
 import progress from '../img/progress.png';
 
 const ItemBook = (props) => {
-  const { title, author, categories } = props;
+  const dispatch = useDispatch();
+
+  const {
+    id, title, author, categories,
+  } = props;
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <div className="book-container">
       <div className="book-info">
@@ -14,16 +25,17 @@ const ItemBook = (props) => {
         </div>
         <div>
           <button type="button">Comments</button>
-          <button type="button">Remove</button>
+          <button type="button" onClick={handleRemoveBook}>Remove</button>
           <button type="button">Edit</button>
         </div>
       </div>
       <div className="book-stat">
+
         <div>
           <img className="progress-circle-bar" alt="progress-circle-bar" src={progress} />
         </div>
         <div>
-          <span>74%</span>
+          <span>64%</span>
           <span>Completed</span>
         </div>
 
@@ -38,6 +50,7 @@ const ItemBook = (props) => {
 };
 
 ItemBook.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   categories: PropTypes.string.isRequired,
